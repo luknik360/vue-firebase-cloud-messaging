4b9ebd330962643be9636324767da4dc11f95963
Łukasz Żukowski <luknik360@gmail.com>
2019-01-29 15:32:58 +0100

    Clear specific data from firebase config files

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,12 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/5.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.8.1/firebase-messaging.js');
 
 var config = {
-  apiKey: "AIzaSyBwJiiGEPL0GEaIQ2LY9XHNgGVV3OZhZ7U",
-  authDomain: "pwa-test-766e2.firebaseapp.com",
-  databaseURL: "https://pwa-test-766e2.firebaseio.com",
-  projectId: "pwa-test-766e2",
-  storageBucket: "pwa-test-766e2.appspot.com",
-  messagingSenderId: "829123798526"
+  apiKey: "<YOUR-API-KEY>",
+  authDomain: "<YOUR-AUTH-DOMAIN>.firebaseapp.com",
+  databaseURL: "https://<YOUR-DB-URL>.firebaseio.com",
+  projectId: "<YOUR-PROJECT-ID>",
+  storageBucket: "<YOUR-STORAGE-BUCKET>",
+  messagingSenderId: "<YOUR-MESSAGING-SENDER-ID>"
 };
 
 firebase.initializeApp(config);
--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,18 @@ import firebase from "firebase/app";
 import 'firebase/messaging';
 
 var config = {
-  apiKey: "AIzaSyBwJiiGEPL0GEaIQ2LY9XHNgGVV3OZhZ7U",
-  authDomain: "pwa-test-766e2.firebaseapp.com",
-  databaseURL: "https://pwa-test-766e2.firebaseio.com",
-  projectId: "pwa-test-766e2",
-  storageBucket: "pwa-test-766e2.appspot.com",
-  messagingSenderId: "829123798526"
+  apiKey: "<YOUR-API-KEY>",
+  authDomain: "<YOUR-AUTH-DOMAIN>.firebaseapp.com",
+  databaseURL: "https://<YOUR-DB-URL>.firebaseio.com",
+  projectId: "<YOUR-PROJECT-ID>",
+  storageBucket: "<YOUR-STORAGE-BUCKET>",
+  messagingSenderId: "<YOUR-MESSAGING-SENDER-ID>"
 };
 
 firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
-messaging.usePublicVapidKey('BCsjNcc5gR-hDeBPNYIydzPpfzP9mUKsnKBJ94kVniq6-OuX57mYsnelh3wZXKHWx-GYwe340PzHDn2RN-qAp70');
+messaging.usePublicVapidKey('<YOUR-PUBLIC-KEY>');
 
 messaging.requestPermission().then(() => {
   messaging.getToken().then(token => {
